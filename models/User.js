4efc291d7f9696ef854d9956f69f772d98530bbd
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-12-16 10:22:11 +0300

    fix sms module

--- models/User.js
+++ models/User.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  phone: { type: String, unique: true, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  phone: { type: String, unique: true, required: true },
   dateOfBirth: { type: Date },
   gender: { type: String },
   verificationCode: { type: String },
   isVerified: { type: Boolean, default: false },
+  password: { type: String, required: true },
+});
+
+// Hash the password before saving
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('User', userSchema);
